4046f713afb32809f91ca1cd3c34cc09a85f1c8d
gem202300 <gemass2023@gmail.com>
2025-05-16 15:03:10 +0000

    button

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex justify-between items-center">
-            <h2 class="text-xl font-semibold text-gray-800">Список пристроїв</h2>
+            <h2 class="text-xl font-semibold text-gray-800">Device</h2>
 
             @can('device_manage')
-                <a href="{{ route('devices.create') }}"
-                   class="bg-blue-600 hover:bg-blue-500 text-white px-4 py-2 rounded transition">
-                    ➕ Додати пристрій
-                </a>
+               <a href="{{ route('devices.create') }}"
+                class="border border-gray-400 text-gray-700 dark:border-gray-600 dark:text-gray-300 hover:bg-gray-200 dark:hover:bg-gray-700 px-4 py-2 rounded transition">
+                  Dodaj urządzenie
+              </a>
             @endcan
         </div>
     </x-slot>
